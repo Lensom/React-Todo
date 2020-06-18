e5b6b7b3d022c16897608dc2abb911995f42a43b
Alexey Rybalko <magurawork@gmail.com>
2020-06-18 09:46:47 +0300

    Added css to project

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import './todo-list-item.css';
 
 const TodoListItem = ({ label, important = false }) => {
 
     const style = { 
-        color: important ? 'tomato' : 'black'
+        color: important ? 'steelblue' : 'black',
+        fontWeight: important ? 'bold' : 'normal'
     }
     return (
-        <span style={style}>{label}</span>
+        <span className="todo-list-item">
+            <span
+                className="todo-list-item-label"
+                style={style}>
+                {label}
+            </span>
+
+            <button type="button" className="btn btn-outline-success btn-sm"><i className="fa fa-exclamation"></i></button>  
+            <button type="button" className="btn btn-outline-danger btn-sm"><i className="fa fa-trash-o"></i></button>  
+        </span>
     )
 }
 
