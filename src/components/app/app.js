9ead2c1ecb656329bf9a514de547ba8db6562ce4
Lensom <magurawork@gmail.com>
2020-06-28 19:52:23 +0300

    Added btn-add, refactor

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,29 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import TodoList from '../todo-list';
 import SearchPanel from '../search-panel';
 import AppHeader from '../app-header';
 import ItemStatusFilter from '../item-status-filter';
+import ItemAddForm from '../item-add-form';
 
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
 
-  const todoData = [
-    { id: 1, label: 'Drink Coffee', important: false },
-    { id: 2, label: 'Make awesome app', important: true },
-  ];
+  maxId = 100;
 
-  return (
-    <div className="wrapper">
-      <AppHeader toDo={3} done={1} />
-      <div className="todo__panel">
-        <SearchPanel />
-        <ItemStatusFilter />
+  state = {
+    todoData: [
+      { id: 1, label: 'Drink Coffee', important: false },
+      { id: 2, label: 'Make awesome app', important: true },
+    ]
+  };
+
+  deleteItem = (id) => {
+    this.setState(({todoData}) => {
+      const idx = todoData.findIndex((el) => el.id === id);
+
+      const newArray =  [...todoData.slice(0, idx), ...todoData.slice(idx + 1)];
+      return {
+        todoData: newArray
+      }
+    })
+  };
+
+  additem = (text) => {
+    //  generate id ?
+    const newItem = {
+      label: text,
+      important: false,
+      id: this.maxId++
+    }
+
+    this.setState(({ todoData }) => {
+      const newArr = [
+        ...todoData,
+        newItem
+      ]
+      return {
+        todoData: newArr
+      }
+    })
+
+
+  }
+
+  onToggleImportant = (id) => {
+    console.log('asd')
+  }
+
+  onToggleDone = (id) => {
+    console.log('as1d')
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <AppHeader toDo={3} done={1} />
+        <div className="todo__panel">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+        <TodoList todos={this.state.todoData} onDeleted={this.deleteItem} onToggleImportant={this.onToggleImportant} onToggleDone={this.onToggleDone} />
+        <ItemAddForm onItemAdded={this.additem}/>
       </div>
-      <TodoList todos={todoData} />
-    </div>
-  )
-}
-
-export default App;
+    )
+  }
+};
--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,26 +3,8 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
-    onLabelClick = () => {
-        this.setState({
-            done: true
-        });
-    };
-
-    onMarkImportant = () => {
-        this.setState({
-            important: true
-        });
-    };
-
-    state = {
-        done: false,
-        important: false
-    }
-
     render() {
-        const { label } = this.props;
-        const { done, important } = this.state;
+        const { label, onDeleted, onToggleImportant, onToggleDone, important, done } = this.props;
 
         let classNames = 'todo-list-item';
         if (done) {
@@ -36,14 +18,14 @@ export default class TodoListItem extends Component {
             <span className={classNames}>
                 <span
                     className="todo-list-item-label"
-                    onClick={this.onLabelClick}
+                    onClick={onToggleDone}
                 >
                     {label}
                 </span>
 
                 <div className="todo__btn--wrapper">
-                    <button type="button" onClick={this.onMarkImportant} className="btn btn-outline-success btn-sm"><i className="fa fa-exclamation"></i></button>
-                    <button type="button" className="btn btn-outline-danger btn-sm"><i className="fa fa-trash-o"></i></button>
+                    <button type="button" onClick={onToggleImportant} className="btn btn-outline-success btn-sm"><i className="fa fa-exclamation"></i></button>
+                    <button type="button" onClick={onDeleted} className="btn btn-outline-danger btn-sm"><i className="fa fa-trash-o"></i></button>
                 </div>
             </span>
         );
